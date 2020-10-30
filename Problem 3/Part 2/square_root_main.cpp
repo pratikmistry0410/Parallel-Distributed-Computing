38109c87a6a5398ff8911962340cf76aa5637657
Learning <learning@suketuvsmacbook.home>
2020-10-29 21:55:10 -0400

    AVX Code

--- Problem 3/Part 2/square_root_main.cpp
+++ Problem 3/Part 2/square_root_main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]){
 	float* result = new float[totalNum];
 
     if (argc < 2) {
-        fprintf(stderr, "ERROR: You must pass number of threads for executing ISPC code with launch tasks in the arguments..!!\n");
+        fprintf(stderr, "\nERROR: You must pass number of threads for executing ISPC code with launch tasks in the arguments..!!\n");
         exit(EXIT_FAILURE);
     }
 
