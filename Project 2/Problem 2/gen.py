25b94226ccd790500d64663bce47be9fe599312e
Learning <learning@suketuvsmacbook.home>
2020-12-02 23:55:29 -0500

    Added support for #,&,%,$ characters in password

--- Project 2/Problem 2/gen.py
+++ Project 2/Problem 2/gen.py
@@ -1,8 +1,9 @@
 import itertools
 # x = [range(48,58) + range(65,91) + range(97,123)]
-x = list(range(48,58))
-x += list(range(65,91))
-x += list(range(97,123))
+x = list(range(35,39))								# Characters - #, $, %, & 
+x += list(range(48,58))								# Characters - 0 to 9
+x += list(range(64,91))								# Characters - @ to Z
+x += list(range(97,123))							# Characters - a to z
 chars = list(map(lambda x: chr(x), x))
 print(chars)
 print(len(chars))
