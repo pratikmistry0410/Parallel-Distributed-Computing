e55c440ced10f486037b8f1d1c257a19af3264cf
Learning <learning@suketuvsmacbook.home>
2020-12-03 00:20:08 -0500

    Removing more ASCII characters

--- Project 2/Problem 2/gen.py
+++ Project 2/Problem 2/gen.py
@@ -1,8 +1,6 @@
 import itertools
-# x = [range(48,58) + range(65,91) + range(97,123)]
-x = list(range(35,39))								# Characters - #, $, %, & 
+x = list(range(35,36))								# Characters - # 
 x += list(range(48,58))								# Characters - 0 to 9
-# x += list(range(64,91))								# Characters - @ to Z
 x += list(range(97,123))							# Characters - a to z
 chars = list(map(lambda x: chr(x), x))
 print(chars)
